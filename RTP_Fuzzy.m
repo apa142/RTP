94cbc365aebd13282196d18a7940a89ebdfd50a3
ap1904 <56799537+ap1904@users.noreply.github.com>
2020-04-06 18:22:41 -0600

    Add files via upload

---
+++ RTP_Fuzzy.m
@@ -0,0 +1,6 @@
+%% It solves Restaurant Tipping Problem using Fuzzy Logic
+% if service is poor or the food is bad, then tip is low
+% if service is good, then tip is average
+% if service is excellent or food is delicious, then tip is generous (high)
+
+gensurf(readfis('tipper')); %solves the complex problem in one line using fuzzy logic
